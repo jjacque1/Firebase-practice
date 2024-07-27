841bb3ee27e12d25ac7eeeea5e74de6131a66d0c
jjacque1 <jacksonjacque.jj@gmail.com>
2024-07-27 14:53:11 -0400

    practice

--- src/Firebase/init.js
+++ src/Firebase/init.js
@@ -1,6 +1,6 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-import { geyAuth } from "firebase/auth";
+import { getAuth } from "firebase/auth";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
@@ -16,4 +16,4 @@ const firebaseConfig = {
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
-const auth = getAuth(); "remove this parenthesis"
+export const auth = getAuth(); 
