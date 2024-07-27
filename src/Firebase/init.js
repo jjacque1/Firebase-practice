60d5abb87c834005e90421c465951662d0ab046f
jjacque1 <jacksonjacque.jj@gmail.com>
2024-07-27 14:10:05 -0400

    firebase practice

--- src/Firebase/init.js
+++ src/Firebase/init.js
@@ -1,6 +1,6 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-
+import { geyAuth } from "firebase/auth";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
@@ -11,8 +11,9 @@ const firebaseConfig = {
   projectId: "library-website-3fe50",
   storageBucket: "library-website-3fe50.appspot.com",
   messagingSenderId: "884177706631",
-  appId: "1:884177706631:web:3891071ee6c31c1fb7a222",
+  appId: "1:884177706631:web:3891071ee6c31c1fb7a222"
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
+const auth = getAuth();
