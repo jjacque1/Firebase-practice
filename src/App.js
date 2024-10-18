1307dfd95e250c25706230fe4b43dbf575b825c1
jjacque1 <jacksonjacque.jj@gmail.com>
2024-10-17 20:14:46 -0400

    Welcom back

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
+import React from 'react';
 import "./App.css";
 import { auth } from "./Firebase/init";
-import { createUserWithEmailAndPassword } from "firebase/auth";
+import {
+  createUserWithEmailAndPassword,
+  signInWithEmailAndPassword,
+  signOut
+} from "firebase/auth";
+
 function App() {
   function register() {
-    createUserWithEmailAndPassword(auth, "email@email.com", "test123")
+    createUserWithEmailAndPassword(auth, "jrstrother@gmail.com", "jrpassword")
       .then((user) => {
         console.log(user);
       })
@@ -11,9 +17,26 @@ function App() {
         console.log(error);
       });
   }
+
+  function login() {
+    signInWithEmailAndPassword(auth, "jrstrother@gmail.com", "jrpassword")
+      .then((user) => {
+        console.log(user);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  }
+
+  function logout() {
+    signOut(auth)
+  }
+
   return (
     <div className="App">
       <button onClick={register}>Register</button>
+      <button onClick={login}>Login</button>
+      <button onClick={logout}>Logout</button>
     </div>
   );
 }
